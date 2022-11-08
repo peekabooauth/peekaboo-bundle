1ea98f1ac98b885fc70b8a9be9b6e912f0d99ef9
vl <vladimir.luchaninov@gmail.com>
2022-11-08 11:42:36 +0100

    don't break if no Request (for example in console commands)

--- src/Services/TokenStorage.php
+++ src/Services/TokenStorage.php
@@ -3,22 +3,25 @@
 namespace Peekabooauth\PeekabooBundle\Services;
 
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class TokenStorage
 {
     private SessionInterface $session;
 
-    public function __construct(private RequestStack $requestStack, private string $tokenName)
-    {
-        $this->session = $this->requestStack->getSession();
+    public function __construct(
+        private readonly RequestStack $requestStack,
+        private readonly string $tokenName,
+    ) {
+        $this->session = $this->requestStack?->getSession() ?? new Session();
     }
 
     public function storageToken(): bool
     {
-        $request = $this->requestStack->getCurrentRequest();
+        $request = $this->requestStack?->getCurrentRequest();
 
-        if ($request->get($this->tokenName)) {
+        if ($request && $request->get($this->tokenName)) {
             $this->session->set($this->tokenName, $request->get($this->tokenName));
 
             return true;
