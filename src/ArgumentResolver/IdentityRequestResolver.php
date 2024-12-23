387a9d35266c0cd6217fd93c067d070b5804f5a1
Vova Luchaninov <vladimir.luchaninov@gmail.com>
2024-12-23 15:50:15 +0100

    ArgumentValueResolverInterface is deprecated since Symfony 6.2

--- src/ArgumentResolver/IdentityRequestResolver.php
+++ src/ArgumentResolver/IdentityRequestResolver.php
@@ -4,25 +4,24 @@ namespace Peekabooauth\PeekabooBundle\ArgumentResolver;
 
 use Peekabooauth\PeekabooBundle\DTO\IdentityRequestDTO;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Controller\ArgumentValueResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ValueResolverInterface;
 use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-class IdentityRequestResolver implements ArgumentValueResolverInterface
+class IdentityRequestResolver implements ValueResolverInterface
 {
     public function __construct(
         private readonly ValidatorInterface $validator,
     ) {
     }
 
-    public function supports(Request $request, ArgumentMetadata $argument): bool
-    {
-        return $argument->getType() === IdentityRequestDTO::class;
-    }
-
     public function resolve(Request $request, ArgumentMetadata $argument): iterable
     {
+        if ($argument->getType() !== IdentityRequestDTO::class) {
+            return [];
+        }
+
         $identityRequestDTO = new IdentityRequestDTO($request->query->all());
         $errors = $this->validator->validate($identityRequestDTO);
         if (count($errors) > 0) {
